ba28a06bbf641e6eccbdb995e82fdefaf392c099
Leomakey22 <118665201+Leomakey22@users.noreply.github.com>
2024-01-08 21:39:26 +0800

    Add files via upload

---
+++ src/planb/PlanB.java
@@ -0,0 +1,10 @@
+package planb;
+public class PlanB {
+    public static void main(String[] args) {
+      CalculatorMidterm planB = new CalculatorMidterm();
+      planB.setVisible(true);
+      planB.setLocationRelativeTo(null);
+      
+    }
+    
+}
